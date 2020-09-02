8ca0a5932a4748a93a26c36d72a3010517c0b86d
sanshuiii <1093466277@qq.com>
2020-09-02 10:55:30 +0800

    correct gdbt configs

--- GBDT.py
+++ GBDT.py
@@ -5,7 +5,8 @@ from sklearn import linear_model
 import pandas as pd
 from gbdt_config import GBDT_CONFIG
 
-def grid(num_round = 20,num_leaves=20):
+
+def grid(num_round=20, num_leaves=20):
     label2 = label - y1
     train_data = lgb.Dataset(data, label=label2)
     train_data.save_binary('train.bin')
@@ -19,20 +20,20 @@ def grid(num_round = 20,num_leaves=20):
 
 
 if __name__ == '__main__':
-    filename=GBDT_CONFIG.get('filename')
+    filename = GBDT_CONFIG.get('filename')
     data, label = DataProvider('sorted_demo_data').gen_test_data()
-    #data, label = z_score(data, label)
+    # data, label = z_score(data, label)
     data = np.array([[x] for x in data])
     label = np.array(label)
     LR = linear_model.LinearRegression()
     LR.fit(data, label)
     y1 = LR.predict(data)
-    lbegin=GBDT_CONFIG.get('leaf_begin',10)
-    lend=GBDT_CONFIG.get('leaf_end',50)
-    nbegin = GBDT_CONFIG.get('node_begin', 10)
-    nend = GBDT_CONFIG.get('node_end', 50)
-    df=pd.DataFrame(columns=list(range(nbegin,nend)),index=list(range(lbegin,lend)))
-    for i in range(lbegin,lend):
-        for j in range(nbegin,nend):
-            df[i][j]=grid(i,j)
-    df.to_csv('gbdt_search.csv')
+    lbegin = GBDT_CONFIG.get('leaf_begin', 10)
+    lend = GBDT_CONFIG.get('leaf_end', 50)
+    nbegin = GBDT_CONFIG.get('tree_begin', 10)
+    nend = GBDT_CONFIG.get('tree_end', 50)
+    df = np.zeros((lend - lbegin, nend - nbegin))
+    for i in range(lbegin, lend):
+        for j in range(nbegin, nend):
+            df[i-lbegin][j-nbegin] = grid(i, j)
+    np.savetxt("foo.csv", df, delimiter=",", fmt='%.2f')
--- gbdt_config.py
+++ gbdt_config.py
@@ -1,7 +1,7 @@
 GBDT_CONFIG={
     'filename':'sorted_demo_data',
-    'leaf_begin':20,
-    'leaf_end':100,
-    'tree_begin':20,
-    'tree_end':100,
+    'leaf_begin':99,
+    'leaf_end':102,
+    'tree_begin':99,
+    'tree_end':102,
 }
